d99e0230f46ef4e0683f7d18814ce460c41bce21
muskanbhasin-1 <jobsmuskanbhasin@gmail.com>
2026-01-19 22:49:31 -0600

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -20,7 +20,7 @@ GO
 --Drop and recreate the 'Datawarehouse' Database
     IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
     BEGIN
-        ALTER DATABASE DataWarehouse SET SINGLEUSER WITH ROLLBACK IMMEDIATE;
+        ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
         DROP DATABASE DataWarehouse;
     END;
     GO
